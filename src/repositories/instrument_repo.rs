6126d00a742ec1a1c3a1048d88c2ba5e2782ad08
Jason Phan <jase.phan@mail.utoronto.ca>
2025-10-06 12:28:44 -0400

    insert statement

--- src/repositories/instrument_repo.rs
+++ src/repositories/instrument_repo.rs
@@ -32,6 +32,21 @@ impl InstrumentRepository {
     /// let created = repo.create(&instrument).await?;
     /// ```
     pub async fn create(&self, instrument: &Instrument) -> Result<Instrument, Error> {
+        let result = sqlx::query_as!(
+            Instrument, 
+            r#"INSERT INTO instruments (symbol, name, instr_type, currency, exchange, multiplier, min_tick) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *"#
+            
+            instrument.symbol,
+            instrument.name,
+            instrument.instr_type,
+            instrument.currency,
+            instrument.exchange,
+            instrument.multiplier,
+            instrument.min_tick
+            )
+            .fetch_one(&self.pool).await?;
+
+        Ok(result)
     }
 
     /// Retrieve all instruments from the database
