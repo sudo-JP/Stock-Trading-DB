eae25cb07895b3851b8b1cda7b4ae6821236d3da
Jason Phan <jase.phan@mail.utoronto.ca>
2025-10-11 00:06:06 -0400

    update position given a struct

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -8,7 +8,7 @@ pub mod ml_models;
 pub use ml_models::{MLModel, ModelPrediction};
 
 pub mod positions;
-pub use positions::Position;
+pub use positions::{Position, PositionUpdate};
 
 pub mod stock_model;
 pub mod technical_indicators;
--- src/models/positions.rs
+++ src/models/positions.rs
@@ -5,3 +5,10 @@ pub struct Position {
     average_cost: f32, 
     unrealized_pnl: f32
 }
+
+pub struct PositionUpdate {
+    instrument_id: i32, 
+    quantity_change: f32, 
+    trade_price: f32, 
+    commission: f32
+}
