3551ed7a7398008bcc0c3e76d01c834d4171d092
Jason Phan <jase.phan@mail.utoronto.ca>
2025-10-20 01:00:40 -0400

    bug fix for position

--- src/repositories/position_repo.rs
+++ src/repositories/position_repo.rs
@@ -1,4 +1,4 @@
-use crate::models::positions::Position; 
+use crate::models::positions::*; 
 use crate::repositories::prelude_repo::*;
 
 pub struct PositionRepository {
@@ -6,30 +6,16 @@ pub struct PositionRepository {
 }
 
 
-pub struct Position {
-    position_id: i32, 
-    instrument_id: i32, 
-    quantity: f32, 
-    average_cost: f32, 
-    unrealized_pnl: f32
-}
 impl PositionRepository {
-    /*async fn create_posititon(&self, position: &Position) -> Result<>{
-        let position = sqlx::query_as!(
+    pub fn new(pool: PgPool) -> Self {
+        Self { pool }
+    }
 
+    async fn get_position_by_instrument(&self, instrument_id: i32) -> Result<Position, sqlx::Error> {
+        let position = sqlx::query_as::<sqlx::Postgres, Position>(
+            "SELECT * FROM positions WHERE instrument_id = $1;" 
             )
-            .fetch_one(&self.pool) 
-            .await?;
-
-        Ok(position)
-    }*/
-
-    async fn get_position_by_instrument(&self, instrument_id: i32) -> Result<Position, Error> {
-        let position = sqlx::query_as!(
-            Position, 
-            "SELECT * FROM positions WHERE instrument_id = $1;", 
-            instrument_id
-            )
+            .bind(instrument_id)
             .fetch_one(&self.pool)
             .await?;
 
@@ -38,26 +24,25 @@ impl PositionRepository {
 
 
     // Archive the position by setting the quantity to 0 
-    async fn close_position(&self, instrument_id: i32) -> Result<(), Error> {
-        sqlx::query!(
-            "UPDATE positions SET quantity = 0 WHERE positions.instrument_id = $1;", 
-            instrument_id
+    async fn close_position(&self, instrument_id: i32) -> Result<bool, sqlx::Error> {
+        let result = sqlx::query("UPDATE positions SET quantity = 0 WHERE positions.instrument_id = $1;"
             )
+            .bind(instrument_id)
             .execute(&self.pool)
-            .await?
+            .await?;
+
+        Ok(result.rows_affected() > 0)
     }
 
     async fn update_position(&self, update: PositionUpdate) -> Result<Position, Error> {
-        let mut position = sqlx::query_as!(
-            Position, 
-            "SELECT * FROM positions WHERE instrument_id = $1;",
-            update.instrument_id
-            )
+        let position = sqlx::query_as::<sqlx::Postgres, Position>(
+            "SELECT * FROM positions WHERE instrument_id = $1;")
+            .bind(update.instrument_id)
             .fetch_one(&self.pool)
             .await?;
         
 
-        let avg_cost = ((position.quantity * position.average_cost) + (update.quantity_change * update.trade_price) + update.commission) / (position.quantity + update.quantity);
+        let avg_cost = ((position.quantity * position.average_cost) + (update.quantity_change * update.trade_price) + update.commission) / (position.quantity + update.quantity_change);
 
         let updated: Position = Position {
             position_id: position.position_id,
@@ -66,17 +51,16 @@ impl PositionRepository {
 
             average_cost: avg_cost,
 
-            unrealized_pnl: 0 // Change later when we have socket to calculate: Market Value - Total Cost 
+            unrealized_pnl: 0.0 // Change later when we have socket to calculate: Market Value - Total Cost 
         };
         // Use cached price for PnL calculation
 
-        sqlx::query!(
-            "UPDATE positions SET quantity = $1, average_cost = $2, unrealized_pnl = $3 WHERE instrument_id = $4;", 
-            updated.quantity, 
-            updated.average_cost, 
-            updated.unrealized_pnl, 
-            updated.instrument_id
-            )
+        sqlx::query(
+            "UPDATE positions SET quantity = $1, average_cost = $2, unrealized_pnl = $3 WHERE instrument_id = $4;")
+            .bind(updated.quantity)
+            .bind(updated.average_cost)
+            .bind(updated.unrealized_pnl)
+            .bind(updated.instrument_id)
             .execute(&self.pool)
             .await?;
     
