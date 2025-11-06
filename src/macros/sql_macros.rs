bac69a53b3fea93f8899c4510d3b6e0cb1678cfa
Jason Phan <jase.phan@mail.utoronto.ca>
2025-11-06 17:17:03 -0500

    convert struct field to string

--- src/macros/sql_macros.rs
+++ src/macros/sql_macros.rs
@@ -1,10 +1,62 @@
 #[macro_export]
+macro_rules! sql_str {
+    (INSERT, $( $attr:ident ),*) => {
+        {
+
+        }
+    };
+    (UPSERT, $count:expr, $conflict:literal, $table:literal, $( $attr:ident ),*) => {
+        {
+            let mut query = String::from("INSERT INTO "); 
+            query.push_str($table);
+            query.push_str("(");
+
+            $(
+                query.push_str(stringify!($attr))
+                query.push_str(", ");
+            )*
+            query.truncate(query.len() - 2); // removes trailing ", "
+
+            query.push_str(") VALUES (");
+            let n: usize = $count as usize;
+            for i in 1..=n {
+                query.push_str(&format!("${}", i));
+                if i != n {
+                    query.push_str(", "); 
+                }
+            }
+
+            query.push_str(") ON CONFLICT (");
+            query.push_str($conflict); 
+            query.push_str(") DO UPDATE SET ");
+
+            $(
+                let setting = format!("{} = EXCLUDED.{}", stringify!($attr), stringify!($attr));
+                query.push_str(&setting);
+                query.push_str(", ");
+            )*
+
+            query.truncate(query.len() - 2); // removes trailing ", "
+            query.push(';');
+            query 
+        }
+    };
+    (DELETE) => {
+    };
+    (SELECT) => {
+    };
+    ( $($other:tt)* ) => {
+        eprint!("Invalid macro usage");
+    }; 
+}
+
+#[macro_export]
 macro_rules! sql_repo {
     (query, $query:expr, $structure:expr, $pool:expr, $( $attr:ident ),*) => {
         {
             sqlx::query($query)
             $(
-                .bind($structure.$attr)
+                .bind(&$structure.$attr)
             )*
             .execute($pool)
             .await?
@@ -19,8 +71,7 @@ macro_rules! sql_repo {
             )*
         }
     };
-    ( _* ) => {
-
-        return Err(sqlx::Error::Protocol("Invalid Macro usage".into())); 
+    ( $($other:tt)* ) => {
+        eprint!("Invalid macro usage");
     }; 
 }
