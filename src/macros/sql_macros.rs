2b45faa60e2ac8471e6cd48bb1dad2b434585cb4
Jason Phan <jase.phan@mail.utoronto.ca>
2025-11-07 15:46:32 -0500

    tail recursion to count the args

--- src/macros/sql_macros.rs
+++ src/macros/sql_macros.rs
@@ -14,20 +14,44 @@ macro_rules! sql_col {
     };
 }
 
+
+fn sql_placeholders(n: usize) -> String {
+    let mut placeholders = String::from(""); 
+    for i in 1..=n {
+        placeholders.push_str(&format!("${}", i));
+        if i != n {
+            placeholders.push_str(", "); 
+        }
+    }
+    placeholders
+}
+
+/*
+ * This is for tail recursion improvision
+ * */
+#[macro_export]
+macro_rules! count_args_helper {
+    ($acc:expr) => { $acc };
+    ($acc:expr, $x:ident, $( $xs:ident ),* ) => { count_args_helper!($acc + 1, $($xs),*) }
+}
+
+#[macro_export]
+macro_rules! count_args {
+    ( $( $attr:ident ),* ) => { 
+        count_args_helper!(0, $($attr),*)
+    }
+}
+
+
 #[macro_export]
 macro_rules! sql_str {
-    (INSERT, $count:expr, $table:literal, $structure:expr, $( $attr:ident ),*) => {
+    (INSERT, $table:literal, $structure:expr, $( $attr:ident ),*) => {
         {
             let mut query = sql_col!($table, $($attr),*);
 
             query.push_str(") VALUES (");
-            let n: usize = $count as usize;
-            for i in 1..=n {
-                query.push_str(&format!("${}", i));
-                if i != n {
-                    query.push_str(", "); 
-                }
-            }
+            let n: usize = count_args!($($attr),*);
+            query.push_str(sql_placeholders(n)); 
             query.push_str(");");
             query 
 
